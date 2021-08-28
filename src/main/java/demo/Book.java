b0dd1a1452fd2607ab634cfc4c6bcfd1711aa388
Apirat.Par <apirat.par@turbo.co.th>
2021-08-28 15:04:25 +0700

    init project

---
+++ src/main/java/demo/Book.java
@@ -0,0 +1,43 @@
+package demo;
+
+public class Book {
+    private int id;
+    private String name;
+    private int price;
+    private int stock;
+
+    public Book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
