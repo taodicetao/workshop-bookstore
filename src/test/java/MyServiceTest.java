1d9984e40f9fe357c67166e709cc71dc9575e60a
Apirat.Par <apirat.par@turbo.co.th>
2021-08-28 18:15:21 +0700

    dependency injection

--- src/test/java/MyServiceTest.java
+++ src/test/java/MyServiceTest.java
@@ -1,11 +1,23 @@
 import org.junit.jupiter.api.Test;
 import solid.MyService;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Random7 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return 7;
+    }
+}
 
 public class MyServiceTest {
     @Test
     public void generateUUID () {
+        Random random = new Random7();
         MyService myService = new MyService();
+        myService.setRandom(random);
         assertEquals("DEMO7", myService.generateUUID());
     }
 }
